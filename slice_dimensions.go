b85c6c20995b9a895e3f1be3932435961e02befb
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-26 12:34:33 -0700

    .Width() & .Height()

---
+++ slice_dimensions.go
@@ -0,0 +1,11 @@
+package text32x36
+
+// Width returns the number of columns.
+func (receiver Slice) Width() int {
+	return Width
+}
+
+// Height returns the number of rows.
+func (receiver Slice) Height() int {
+	return Height
+}
