5904497dd631401c0172e04957628ff280fbf5e3
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-07-26 22:38:57 -0700

    added to docs

--- slice_dimensions.go
+++ slice_dimensions.go
@@ -1,11 +1,17 @@
 package text32x36
 
 // Width returns the number of columns.
+//
+// Note that this is NOT the width of the image.Image that this represents.
+// For image.Image width information use the Bounds method.
 func (receiver Slice) Width() int {
 	return Width
 }
 
 // Height returns the number of rows.
+//
+// Note that this is NOT the height of the image.Image that this represents.
+// For image.Image height information use the Bounds method.
 func (receiver Slice) Height() int {
 	return Height
 }
